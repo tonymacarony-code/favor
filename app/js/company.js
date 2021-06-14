49fe3392701ca12b62303418645c725c041655ab
tonymacarony-code <74680049+tonymacarony-code@users.noreply.github.com>
2021-06-14 23:27:59 +0300

    add company page

---
+++ app/js/company.js
@@ -0,0 +1,11 @@
+var btnContainer = document.getElementById("aside-menu");
+
+var btns = btnContainer.getElementsByClassName("company__aside-menu_btn");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
--- app/js/slider.js
+++ app/js/slider.js
@@ -7,3 +7,13 @@ $(document).ready(function () {
     autoplaySpeed: 5000,
   });
 });
+
+// $(document).ready(function () {
+//   $(".company__content-rule").slick({
+//     slidesToShow: 1,
+//     dots: true,
+//     arrows: false,
+//     autoplay: true,
+//     autoplaySpeed: 5000,
+//   });
+// });
