2d8d15264c4212e59052486d964520797db4a023
tonymacarony-code <74680049+tonymacarony-code@users.noreply.github.com>
2021-06-17 02:52:09 +0300

    fix company page, add product page, fix bugs

--- app/js/slider.js
+++ app/js/slider.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".company__content-rule_slider").slick({
     slidesToShow: 1,
-    dots: true,
-    arrows: true,
+    dots: false,
+    arrows: false,
     autoplay: true,
     autoplaySpeed: 5000,
   });
@@ -31,3 +31,55 @@ $(".company__content-rule_slider").on(
     sliderBg2.classList.toggle("visible");
   }
 );
+
+$("aside-menu")
+  .find(".company__aside-menu_btn")
+  .click(function (e) {
+    e.preventDefault();
+    var section = $(this).attr("id");
+    $("html, body").animate({
+      scrollTop: $(section).offset().top,
+    });
+  });
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 98,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
