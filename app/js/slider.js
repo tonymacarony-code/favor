c8e93369c44112a3b8e6cc25152c10e5709b3908
tonymacarony-code <74680049+tonymacarony-code@users.noreply.github.com>
2021-06-16 20:34:18 +0300

    fix company page

--- app/js/slider.js
+++ app/js/slider.js
@@ -8,12 +8,26 @@ $(document).ready(function () {
   });
 });
 
-// $(document).ready(function () {
-//   $(".company__content-rule").slick({
-//     slidesToShow: 1,
-//     dots: true,
-//     arrows: false,
-//     autoplay: true,
-//     autoplaySpeed: 5000,
-//   });
-// });
+$(document).ready(function () {
+  $(".company__content-rule_slider").slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+});
+
+$(".company__content-rule_slider").on(
+  "beforeChange",
+  function (event, slick, currentSlide, nextSlide) {
+    var sliderBg1 = document.getElementsByClassName(
+      `slider__bg--${currentSlide}`
+    )[0];
+    var sliderBg2 = document.getElementsByClassName(
+      `slider__bg--${nextSlide}`
+    )[0];
+    sliderBg1.classList.toggle("visible");
+    sliderBg2.classList.toggle("visible");
+  }
+);
