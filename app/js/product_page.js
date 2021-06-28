51a44325fae16211df8e1eb4bbcf4fbee4b2f458
tonymacarony-code <74680049+tonymacarony-code@users.noreply.github.com>
2021-06-28 17:22:45 +0300

    fix bugs

---
+++ app/js/product_page.js
@@ -0,0 +1,58 @@
+var request;
+var $current;
+var cache = {};
+var $frame = $("#photo-viewer");
+var $thumbs = $(".product-page__image");
+
+function crossfade($img) {
+  if ($current) {
+    $current.stop().fadeOut("slow");
+  }
+
+  $img.css({
+    marginLeft: -$img.width() / 2,
+    marginTop: -$img.height() / 2,
+  });
+
+  $img.stop().fadeTo("slow", 1);
+  $current = $img;
+}
+
+$(document).on("click", ".product-page__image", function (e) {
+  var $img,
+    src = this.href;
+  request = src;
+
+  e.preventDefault();
+  $thumbs.removeClass("active");
+  $(this).addClass("active");
+
+  if (cache.hasOwnProperty(src)) {
+    if (cache[src].isLoading === false) {
+      crossfade(cache[src].$img);
+    }
+  } else {
+    $img = $("<img/>");
+    cache[src] = {
+      $img: $img,
+      isLoading: true,
+    };
+
+    $img.on("load", function () {
+      $img.hide();
+      $frame.removeClass("is-loading").append($img);
+      cache[src].isLoading = false;
+      if (request === src) {
+        crossfade($img);
+      }
+    });
+
+    $frame.addClass("is-loading");
+    $img.attr({
+      src: src,
+      alt: this.title || "",
+    });
+  }
+});
+
+$(".product-page__image").eq(0).click();
